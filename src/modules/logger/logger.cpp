4fffe38f38e080c40ff1c03bdad02bd4c0c3fcde
CarlOlsson <carlolsson.co@gmail.com>
2019-01-14 11:18:40 +0100

    update logger

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -627,7 +627,7 @@ void Logger::add_default_topics()
 	add_topic("ekf2_innovations", 200);
 	add_topic("ekf_gps_drift");
 	add_topic("esc_status", 250);
-	add_topic("estimator_status", 200);
+	add_topic("estimator_status", 50);
 	add_topic("home_position");
 	add_topic("input_rc", 200);
 	add_topic("manual_control_setpoint", 200);
